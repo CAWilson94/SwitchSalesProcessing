a9e0290ae673366becc13587bd9ea7dc702a2eae
charlotte <charlotteawilson.94@gmail.com>
2020-02-23 01:24:07 +0000

    Data Generation - message type added

--- data_generator.py
+++ data_generator.py
@@ -1,5 +1,6 @@
 from operator import add, sub, mul
 import numpy as np
+import pandas as pd
 import random
 
 operations = {'Add': add, 'Subtract': sub, 'Multiply': mul}
@@ -23,7 +24,18 @@ class DataGenerator:
     def generate_input_data(self, products):
         products['amount'] = np.random.randint(1, 10, size=len(products))
         products['operation'] = products.apply(self._add_random_operation, axis=1)
-        products['random_val_op'] = np.random.randint(1,5, size=len(products))
+        products['random_val_op'] = np.random.randint(1,6, size=len(products))
         products['adjusted_value'] = products.apply(self._apply_operations, axis=1)
         products['total_value'] = products.apply(self._apply_total_value, axis=1)
+        products['message_type'] = np.random.randint(1,4, size=len(products))
         return products
+
+
+def main():
+    product_input = pd.read_csv('products.csv')
+    dataGenerator = DataGenerator()
+    df = dataGenerator.generate_input_data(product_input)
+    print(df)
+
+if __name__ == '__main__':
+    main()
