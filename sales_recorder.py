da4602ecbc521551cd4e23debfe5fd778f8f5438
charlotte <charlotteawilson.94@gmail.com>
2020-02-27 19:26:40 +0000

    testing report for both basic and end types and running only basic

--- sales_recorder.py
+++ sales_recorder.py
@@ -10,13 +10,11 @@ class SalesRecorder:
         self.sales.append(sale)
         self._sales_logger_check() #rename
 
+
     def _sales_logger_check(self):
         """ Checking when we need to log sales """
         sales_logger = ReportLogger(self.sales)
         if (int(len(self.sales)%10) and int(len(self.sales)< 50)) == 0:
-            print(sales_logger.basic_report())
+            sales_logger.basic_report()
         if (int(len(self.sales) % 50)) == 0:
-            print(sales_logger.end_report())
-        # probably want to store these in a df?
-        # check if sale count is divisible perfectly by 10 -> so when we get 10, 20, 30 etc we will spit out logs
-        # second check for divisible by 50 for second logging
+            sales_logger.end_report()
