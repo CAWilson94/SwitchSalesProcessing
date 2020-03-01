dc13390f57a226008e635f3c336b44255ec79cef
charlotte <charlotteawilson@94.gmail.com>
2020-03-01 21:02:32 +0000

    Adjustments added for sales already recorded

--- message_processor.py
+++ message_processor.py
@@ -7,6 +7,7 @@ class MessageProcessor:
 
     MAX_REPORT = 50
     OPERATIONS = set(["add", "subtract", "multiply"])
+    UNUSED_WORDS = ['of', 'at', 'each']
 
     def __init__(self, input_path):
         self.input_path = input_path
@@ -15,42 +16,61 @@ class MessageProcessor:
     def _parse_message_one(self, word_list): 
         """ Parse messages showing product and value, assuming amount is one per sale. """ 
         try:
-            print(word_list)
+            word_list = [word for word in word_list if word not in self.UNUSED_WORDS]
+            value = float(word_list[-1])
+            product = " ".join(word_list[:-1]).title()
+            sale = Sale(product, value=value)
+            return sale
         except expression as e:
             print("use a logger instead")        
 
     def _parse_message_two(self, word_list): 
         """ Parse messages showing product, value and amount of products in sale. """ 
         try:
-            print(word_list)
+            word_list = [word for word in word_list if word not in self.UNUSED_WORDS]
+            amount = int(word_list[0])
+            value = float(word_list[-1][1:]) # grab end value then string values after the £ sign
+            product = " ".join(word_list[1: -1]).title()
+            sale = Sale(product, amount=amount, value=value)
+            return sale
         except expression as e:
             print("use a logger instead")
 
-    def _parse_message_three(self, word_list): 
+    def _apply_adjustments(self, word_list): 
         """ Parse messgage that shows adjustments for product. """ 
         try:
             operation = word_list[0]
-            amount = word_list[1][1:] # gives us item 1 from list then index 1 onwards for this (skipping the £)
-            product = " ".join(word_list[2:]) # allowing for spaces in product namne
-            sale = Sale(product)
-            sale.add_adjustment(operation, amount)
-            return sale
+            amount = float(word_list[1][1:]) # gives us item 1 from list then index 1 onwards for this (skipping the £)
+            product = " ".join(word_list[2:]).title() # allowing for spaces in product namne
+            """ 
+            instead of saving a new sale you want to loop through 
+            all current sales with the same product name and add this adjustment to them.. 
+        
+            """ 
+            for sale in self.sales_recorder.sales: 
+                if sale.product.lower() == product.lower(): 
+                    sale.add_adjustment(operation, amount)
+                       
         except Exception as e:
             logger.warn("Cannot parse adjustment message: {}".format(e))
 
-    def parse_messages(self): 
-        """ Determine message type and parse """
+    def process_messages(self): 
+        """ Determine message type and process into sales """
         input_file = open(self.input_path, 'r')
         for line in input_file:             
             word_list = line.lower().split()
             if self.OPERATIONS.intersection(word_list):
-                self._parse_message_three(word_list) #  message three being the one with adjustments 
+                self._apply_adjustments(word_list) #  message three being the one with adjustments                                 
             elif word_list[0].isdigit(): 
-                self._parse_message_two(word_list) # message two is amount of product message
-            elif (len(word_list) == 3) and (word_list[2].isdigit) and (type(word_list[0]) == str): # message one is basic type and value 
-                self._parse_message_one(word_list)
+                sale = self._parse_message_two(word_list) # message two is amount of product message
+                self.sales_recorder.record_sale(sale)
+            elif(word_list[-1].isdigit): # message one is basic type and value 
+                sale = self._parse_message_one(word_list)
+                self.sales_recorder.record_sale(sale)
             else: 
                 print(f"input: {line} could not be processed.")                                 
+                continue # probably don't want to store NO sale 
+            
 
     def process_sales(self):
         """ Get sales from the input given """
@@ -63,7 +83,7 @@ def main():
     #input = pd.read_csv('generated_product_data.csv')
     input_path = "input_messages.txt"
     mp = MessageProcessor(input_path)
-    mp.parse_messages()
+    mp.process_messages()
 
 if __name__ == '__main__':
     main()
