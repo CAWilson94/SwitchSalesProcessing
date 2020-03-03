3ea3af084725b7446c44732a1951c2c17c2d43ca
charlotte <charlotteawilson.94@gmail.com>
2020-03-03 10:40:06 +0000

    message processing app tests for basic, end report and no input file working.

--- tests/test_message_processing_app.py
+++ tests/test_message_processing_app.py
@@ -1,5 +1,7 @@
 import mock
 from unittest import TestCase
+
+from sale import  Sale
 from message_processing_app import MessageProcessingApp
 
 
@@ -8,26 +10,43 @@ class TestMessageProcessingApp(TestCase):
     def setUp(self):
         self.input_test_path = "test_input_messages.txt"
         self.mpa = MessageProcessingApp(self.input_test_path)
+        basic_report__mock_input = create_test_input()
+        self.data = "/n".join(basic_report__mock_input)
 
     @mock.patch('message_processing_app.Report')
     @mock.patch('message_processing_app.logging')
     @mock.patch('message_processing_app.MessageProcessor')
-    def test_process_sales_messages(self, mock_message_processor, mock_logging,
-                                    mock_report):
-        """ Lets test the happy paths first. """
-        basic_report__mock_input = create_test_input()
-        data = "/n".join(basic_report__mock_input)
+    def test_basic_report_process_sales_messages(self, mock_message_processor, mock_logging, mock_report):
+        """ Testing specifically basic report. """
+        test_sale = mock.MagicMock()
+        self.mpa.sales_list = [test_sale] * 10
         self.mpa.number_messages = mock.MagicMock()
-        self.mpa._prepare_report = mock.MagicMock()
-        with mock.patch("builtins.open", mock.mock_open(read_data=data)) as mock_file:
+        mock_message_processor.return_value.sales_list = [test_sale] * 10
+        with mock.patch("builtins.open", mock.mock_open(read_data=self.data)) as mock_file:
             self.mpa.number_messages = 10
             self.mpa.process_sales_messages()
-            mock_message_processor.return_value.process_message.assert_called_with(data)
-            self.mpa.sales_list = basic_report__mock_input
-            self.mpa._prepare_report.assert_any_call()
-            self.mpa._prepare_report.sales_list[0].apply_adjustments.assert_any_call()
-            mock_report.log_basic_report.assert_any_call()
+            mock_message_processor.return_value.process_message.assert_called_with(self.data)
+            mock_report.return_value.log_basic_report.assert_any_call()
+            self.mpa.sales_list[0].apply_adjustments.assert_any_call()
 
+
+    @mock.patch('message_processing_app.Report')
+    @mock.patch('message_processing_app.logging')
+    @mock.patch('message_processing_app.MessageProcessor')
+    def test_end_report_process_sales_messages(self, mock_message_processor, mock_logging, mock_report):
+        """ Testing specifically end report. """
+        test_sale = mock.MagicMock()
+        self.mpa.sales_list = [test_sale] * 10
+        self.mpa.number_messages = mock.MagicMock()
+        with mock.patch("builtins.open", mock.mock_open(read_data=self.data)) as mock_file:
+            self.mpa.number_messages = 50
+            self.mpa.process_sales_messages()
+            mock_report.return_value.log_end_report.assert_any_call()
+
+    @mock.patch('message_processing_app.Report')
+    @mock.patch('message_processing_app.logging')
+    @mock.patch('message_processing_app.MessageProcessor')
+    def test_process_sales_messages_no_input(self, mock_message_processor, mock_logging, mock_report):
         with self.assertRaises(Exception) as context:
             self.mpa.process_sales_messages()
             expected_error = "[Errno 2] No such file or directory: 'test_input_messages.txt'"
